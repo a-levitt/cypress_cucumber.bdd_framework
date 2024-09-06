d7bae4aec7953d0f18cb950d288cbbe414267c47
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-06 23:14:47 +0200

    Implement steps for Contact us page

--- cypress/support/step_definitions/ContactUsPage_Steps.js
+++ cypress/support/step_definitions/ContactUsPage_Steps.js
@@ -3,21 +3,21 @@ import {When} from "@badeball/cypress-cucumber-preprocessor";
 
 
 When(`Fill in First Name field`, () => {
-
+    cy.get("[name='first_name']").type("John");
 })
 
 When(`Fill in Last Name field`, () => {
-
+    cy.get("[name='last_name']").type("Doe");
 })
 
 When(`Fill in Email Address field`, () => {
-
+    cy.get("[name='email']").type("test.email@cypress.com");
 })
 
 When(`Fill in Comment field`, () => {
-
+    cy.get("[name='message']").type("Lorem ipsum comment.");
 })
 
 When(`Click Submit button`, () => {
-
+    cy.get("[type='submit']").click();
 })
