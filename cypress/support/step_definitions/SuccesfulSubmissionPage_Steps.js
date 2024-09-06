fbe98d61ab83f327e08b48d915714d2725b1f8db
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-06 23:21:15 +0200

    Implement assertion for last step

--- cypress/support/step_definitions/SuccesfulSubmissionPage_Steps.js
+++ cypress/support/step_definitions/SuccesfulSubmissionPage_Steps.js
@@ -2,5 +2,5 @@ import {Then} from "@badeball/cypress-cucumber-preprocessor";
 
 
 Then('Successful submission message is displayed', () => {
-
+    cy.get("h1").should("have.text", "Thank You for your Message!");
 })
